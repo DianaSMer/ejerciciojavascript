6dc5188eed44ca9503e67af4321929c7f6205cdc
DianaSMer <anaidmerloi@gmail.com>
2024-01-09 12:56:42 -0600

    Agregar funciones

--- script.js
+++ script.js
@@ -110,3 +110,27 @@ console.log (isEarthRoundNumber);
 
 
 
+//Calcular el perimetro de un circulo
+function calculaElPerimetroCirculo(d){return d * 3.1416;
+}
+
+//Calcular el área de un rectangulo
+function calculaElAreaRectangulo (b,h){return b * h;
+}
+
+//Calcula el cuadrado de un numero
+
+function calculaElCuadrado (x) {return x*x;
+}
+
+//Calcula la conversion de celcius a farenheit 
+
+function conversionCelsiusF (C) {return C* 9 / 5 + 32;
+}
+
+//Calcula el valor del voltaje dadas la resistencia y la corriente
+const calculaVoltaje = (I, R) => (I * R);
+
+//Calcula el volumen de una esfera
+
+function calcularVolumenDeEsfera (r) {return 4/3 * 3.1416 * r * r +r;}
