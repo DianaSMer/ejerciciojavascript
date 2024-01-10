3efe58e04711b6de17e7c5389ae18fe66acd2e5a
DianaSMer <anaidmerloi@gmail.com>
2024-01-09 20:09:17 -0600

    ejerciciocontroldeflujo

---
+++ control.js
@@ -0,0 +1,40 @@
+//Ejercicio 1 mayor de edad
+let n= 30
+if (n>=18){
+    console.log("La persona es mayor de edad")
+} else {
+    console.log("La persona es menor de edad")
+
+}
+
+//Ejercicio 2 calcular el promedio de un alumno
+
+
+//let promedioAlumno = (function promedioTotal(a, b, c, d) {return `suma es ${(a +b + c + d) / 4}`;})
+
+let mate = 10
+let espanol = 6
+let geografia = 10
+;
+let promedio = (mate + espanol + geografia) / 3;
+if (promedio >= 7 ) {
+    console.log("Aprobado");
+} else {
+    console.log("Reprobado");
+}
+
+let calificacionFinal = promedio >=7 ? "Bien" : "Mal"
+    console.log(calificacionFinal);
+
+
+//Ejercicio 4 calcular el 
+
+let x = 36
+if (x % 4 == 0 && x % 9 == 0) {
+    console.log("El numero es divisible entre 4 y 9")
+} else {
+    console.log("el numero no es divisible entre 4 y 9")
+}
+
+//Ejercicio 7
+
